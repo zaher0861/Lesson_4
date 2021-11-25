d6e4616effde40b76995c8cd0bca78de92e62a07
bagae <bagae@DESKTOP-9CALVO2>
2021-11-25 22:28:46 +0300

    Добавьте файлы проекта.

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давайте найдем квадрат числа N!");
+            Console.Write("Введите целое число больше 0: ");
+            double N = Convert.ToDouble(Console.ReadLine());
+            int i = 1;
+            int NN = 0;
+            if (N > 0 && Math.Floor(N) == N)
+            {
+                for (; i<=N; i++)
+                {
+                    NN = NN + (2*i-1);
+                    Console.WriteLine("Квадрат числа {0} равен {1}", i, NN);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введено неверное число!");
+
+            }
+            Console.WriteLine("Для завершения работы нажмите любую клавишу");
+            Console.ReadKey();
+
+        }
+    }
+}
